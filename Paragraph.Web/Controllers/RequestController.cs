b58851d3af568d09163a0f423ae7be20f3f7f24e
Dilyana <dilyana_stz@abv.bg>
2019-01-26 06:50:43 +0200

    Controllers

---
+++ Paragraph.Web/Controllers/RequestController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Paragraph.Web.Controllers
+{
+    using Services.DataServices;
+    using Data.Models;
+    using Services.DataServices.Models.Request;
+
+    public class RequestController : Controller
+    {
+
+        private readonly UserManager<ParagraphUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IUserService userService;
+        private readonly IRequestService requestService;
+
+        public RequestController(UserManager<ParagraphUser> userManager, RoleManager<IdentityRole> roleManager, IUserService userService, IRequestService requestService)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            this.userService = userService;
+            this.requestService = requestService;
+        }
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //[Authorize(Roles ="Admin")]
+        //public async Task<IActionResult> SetModeratorRole()
+        //{
+        //    await roleManager.CreateAsync(new IdentityRole("Writer"));
+        //    return this.Content($"Created role Writer");
+        //}
+
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult MakeRequest(RequestModel model)
+        {
+            if(!roleManager.RoleExistsAsync(model.Role).Result)
+            {
+                this.ViewData["Error"] = "Please choose a valid role!";
+                return this.RedirectToAction("Profile", "User");
+            };
+            var role = this.roleManager.FindByNameAsync(model.Role).Result;
+            this.requestService.MakeRequest(this.User.Identity.Name, role);
+
+            return this.RedirectToAction("Profile", "User");
+        }
+
+        [Authorize(Roles="Admin")]
+       // [ValidateAntiForgeryToken]
+        public IActionResult GrantRequest(int id)
+        {
+            var roleName = this.requestService.GetRole(id);
+            var requestSenderId = this.requestService.GetUserId(id);
+
+           bool success = this.requestService.GrantRequest(requestSenderId, roleName).Result;
+            if(success)
+            {
+                this.requestService.SetRequestStatus(id);
+            }
+
+            return this.RedirectToAction("Profile", "User");
+        }
+
+       
+    }
+}
---
+++ Paragraph.Web/Controllers/TagController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Paragraph.Web.Controllers
+{
+    using Services.DataServices;
+    using Services.DataServices.Models.Tag;
+
+    public class TagController : Controller
+    {
+        private readonly ITagService tagService;
+        private readonly IArticleService articleService;
+
+        public TagController(ITagService tagService, IArticleService articleService)
+        {
+            this.tagService = tagService;
+            this.articleService = articleService;
+        }   
+     
+        public IActionResult Details(int id)
+        {
+            if(!tagService.IsTagValid(id))
+            {
+                return this.RedirectToAction("All", "Tag");
+            }
+
+            var tag = tagService.Details(id);
+            return this.View(tag);
+        }
+
+        public IActionResult All()
+        {
+            var model = this.tagService.All();
+
+            return this.View(model);
+        }
+
+        [Authorize(Roles="Admin, Moderator")]
+        public IActionResult Create() => this.View();
+
+        [Authorize(Roles ="Admin, Moderator")]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult Create(CreateTagModel tag)
+        {
+            if(!this.ModelState.IsValid)
+            {
+                return this.View();
+            }
+            this.tagService.Create(tag);
+            return this.RedirectToAction("All", "Tag");
+        }
+
+        [Authorize(Roles ="Admin, Moderator")]
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public IActionResult AddTag(AddTagModel model, int id)
+        {
+            if(!this.articleService.DoesArticleExist(id))
+            {
+                this.ViewData["Error"] = "Article does not exist.";
+                return this.RedirectToAction("Index", "Home");
+            }
+
+            if (!this.ModelState.IsValid)
+            {
+                return this.RedirectToAction("Details", "Article", new { id = id });
+            }
+            this.tagService.AddTagToArticle(model.TagId, id);
+            return this.RedirectToAction("Details", "Article", new { id = id});
+        }
+
+      
+
+    }
+}
